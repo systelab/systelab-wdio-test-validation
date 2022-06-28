33622fa02805838a2c3f1b34b5892aadb5a0246e
DaniMartinez-Gonzalez <dmartinez1@werfen.com>
2022-06-28 17:35:29 +0200

    findSelectors by id, tagname, css and className

--- app/wdio-validation/pageObjects/showCaseComponentsPage.ts
+++ app/wdio-validation/pageObjects/showCaseComponentsPage.ts
@@ -1,10 +1,27 @@
-import { BasePage } from 'systelab-components-wdio-test';
+import { BasePage, InputField } from 'systelab-components-wdio-test';
 
 export class ShowCaseComponentsPage extends BasePage {
     constructor() {
 		super('showcase-components');
     }
+  
     public async getTextTitleByPosition(position: number): Promise<any>{
-        return this.allByClassName('d-inline').get(position).getText();
+      return this.allByClassName('d-inline').get(position).getText();
+    }
+
+    public getTextBoxByClassName(): InputField {
+      return new InputField(this.byClassName('form-control'));
+    }
+  
+    public getTextBoxByCss(): InputField {
+      return new InputField(this.byCSS('.form-control'));
+    }
+  
+    public getTextBoxById(): InputField {
+      return new InputField(this.byId('full-width-input'));
+    }
+  
+    public getTextBoxByTagName(): InputField {
+      return new InputField(this.byTagName('input'));
     }
 }
